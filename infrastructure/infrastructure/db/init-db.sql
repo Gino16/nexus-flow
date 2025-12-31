51361bbb6ffb3431831d78b5405b04e98d02b2e9
Gino Ascencio <gino.francisco@hotmail.com>
2025-12-31 18:14:05 -0500

    add other foldes

---
+++ infrastructure/infrastructure/db/init-db.sql
@@ -0,0 +1,8 @@
+-- Base de datos para la gestión de identidad (Keycloak)
+CREATE DATABASE keycloak_db;
+
+-- Base de datos para nuestro microservicio de ingesta
+CREATE DATABASE nexusflow;
+
+-- Opcional: Podrías crear una para el microservicio de analítica más adelante
+CREATE DATABASE nexusflow_analytics;
